a3134558315c36e15a48145e59941134fffdacf5
Aimee Knight <AimeeKnight83@gmail.com>
2013-08-27 13:56:10 -0400

    Change app name to enable mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Saved::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'whatever-heroku-gives-you.herokuapp.com'} 
+  config.action_mailer.default_url_options = { :host => 'saved.herokuapp.com'} 
    
   config.action_mailer.delivery_method = :smtp
    
